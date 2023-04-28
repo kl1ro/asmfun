c60e75be9c65f06dbe51d71e0226fc9da86ea340
MindW1n <ivan.prosworker@gmail.com>
2023-04-28 09:23:31 +0300

    Add some comments

--- Headers16bit/SwitchToLM/main.asm
+++ Headers16bit/SwitchToLM/main.asm
@@ -20,14 +20,16 @@ mov edi, 0x1000
 ; PDT   -> 0x3000
 ; PT    -> 0x4000
 
-; Filling
+;
+; Filling the page tables
+;
 mov dword [edi], 0x2003
 shl edi, 1
 mov dword [edi], 0x3003
 or edi, 0x1000
 mov dword [edi], 0x4003
-add edi, 0x1000
 
+add edi, 0x1000
 mov ebx, 3
 mov ecx, 0x200
 
@@ -39,7 +41,8 @@ mov ecx, 0x200
 
 ;
 ; Set 5th bit in control reg 4
-; Meaning Paging is on
+; Meaning the physical address
+; extension is on
 ;
 mov eax, cr4
 or eax, 1 << 5
@@ -50,14 +53,29 @@ mov cr4, eax
 ; Meaning we have 64 bit address space
 ; but only 16 and 32 bit instructions
 ;
+; Here we set the long mode bit
+;
 mov ecx, 0xc0000080
 rdmsr
 or eax, 1 << 8
 wrmsr
 
+;
+; And here we actually enable the 
+; paging
+;
 mov eax, cr0
 or eax, 0x80000001
 mov cr0, eax
 
 lgdt [GDTLM.Pointer]
+
+;
+; Switch the pointers
+;
+mov ax, GDTLM.Data
+mov ss, ax
+mov ds, ax
+mov es, ax
+
 jmp GDTLM.Code:KERNEL_OFFSET 		; kernel offset
